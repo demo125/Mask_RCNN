a0d055a36bb99e3369b82129e223d146906dab7c
Martin Demeter <demeter19@saturn.dai.fmph.uniba.sk>
2020-01-24 18:34:49 +0100

    first

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -84,7 +84,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
+                      colors=None, captions=None, save_dir=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -163,8 +163,14 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    
+    if save_dir:
+        plt.savefig(save_dir)
+        
     if auto_show:
         plt.show()
+        
+    
 
 
 def display_differences(image,
