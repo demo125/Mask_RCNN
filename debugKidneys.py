b1a5d19b71d3ee55237dbebc59c5c7201e054578
demetermart <demetermart@gmail.com>
2020-01-24 21:59:51 +0100

    qwe

--- debugKidneys.py
+++ debugKidneys.py
@@ -15,107 +15,110 @@ import glob
 
 # class that defines and loads the kangaroo dataset
 class KangarooDataset(Dataset):
-	# load the dataset definitions
-	def load_dataset(self, dataset_dir, is_train=True):
-		self.add_class("dataset", 1, "kidney")
-		
-		files = glob.glob(dataset_dir+ r'\**\*', recursive=True)
-		image_id = 0
-		for f in files:
-			
-			if f.endswith('.jpg'):
-				xml = f.replace('.jpg', '.xml')
-				if not os.path.isfile(xml):
-					xml = os.path.join(dataset_dir, 'empty.xml')
+    # load the dataset definitions
+    def load_dataset(self, dataset_dir, is_train=True):
+        self.add_class("dataset", 1, "kidney")
+        
+        dirname = os.path.dirname(__file__)
+        dataset_dir = os.path.join(dirname, dataset_dir)
 
-				if is_train and int(image_id) >= 180:
-					continue
-				
-				if not is_train and int(image_id) < 180:
-					continue
+        files = glob.glob(dataset_dir+ r'\**\*', recursive=True)
+        image_id = 0
+        for f in files:
+            
+            if f.endswith('.jpg'):
+                xml = f.replace('.jpg', '.xml')
+                if not os.path.isfile(xml):
+                    xml = os.path.join(dataset_dir, 'empty.xml')
 
-				img_path = f
-				ann_path = xml
-				self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
-				
-				image_id += 1
-				
-				# print(f)
-				# print(xml)
-				# print()
-			
-	def old():       
-		# define one class
-		self.add_class("dataset", 1, "kangaroo")
-		# define data locations
-		images_dir = dataset_dir + '/images/'
-		annotations_dir = dataset_dir + '/annots/'
-		# find all images
-		for filename in listdir(images_dir):
-			# extract image id
-			image_id = filename[:-4]
-			# skip bad images
-			if image_id in ['00090']:
-				continue
-			# skip all images after 150 if we are building the train set
-			if is_train and int(image_id) >= 150:
-				continue
-			# skip all images before 150 if we are building the test/val set
-			if not is_train and int(image_id) < 150:
-				continue
-			img_path = images_dir + filename
-			ann_path = annotations_dir + image_id + '.xml'
-			# add to dataset
-			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+                if is_train and int(image_id) >= 180:
+                    continue
+                
+                if not is_train and int(image_id) < 180:
+                    continue
 
-	# extract bounding boxes from an annotation file
-	def extract_boxes(self, filename):
-		# load and parse the file
-		tree = ElementTree.parse(filename)
-		# get the root of the document
-		root = tree.getroot()
-		# extract each bounding box
-		boxes = list()
-		for box in root.findall('.//bndbox'):
-			xmin = int(box.find('xmin').text)
-			ymin = int(box.find('ymin').text)
-			xmax = int(box.find('xmax').text)
-			ymax = int(box.find('ymax').text)
-			coors = [xmin, ymin, xmax, ymax]
-			boxes.append(coors)
-		# extract image dimensions
-		width = int(root.find('.//size/width').text)
-		height = int(root.find('.//size/height').text)
-		return boxes, width, height
+                img_path = f
+                ann_path = xml
+                self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+                
+                image_id += 1
+                
+                # print(f)
+                # print(xml)
+                # print()
+            
+    def old():       
+        # define one class
+        self.add_class("dataset", 1, "kangaroo")
+        # define data locations
+        images_dir = dataset_dir + '/images/'
+        annotations_dir = dataset_dir + '/annots/'
+        # find all images
+        for filename in listdir(images_dir):
+            # extract image id
+            image_id = filename[:-4]
+            # skip bad images
+            if image_id in ['00090']:
+                continue
+            # skip all images after 150 if we are building the train set
+            if is_train and int(image_id) >= 150:
+                continue
+            # skip all images before 150 if we are building the test/val set
+            if not is_train and int(image_id) < 150:
+                continue
+            img_path = images_dir + filename
+            ann_path = annotations_dir + image_id + '.xml'
+            # add to dataset
+            self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
-	# load the masks for an image
-	def load_mask(self, image_id):
-		# get details of image
-		info = self.image_info[image_id]
-		# define box file location
-		path = info['annotation']
-		# load XML
-		boxes, w, h = self.extract_boxes(path)
-		# create one array for all masks, each on a different channel
-		if len(boxes):
-			masks = zeros([h, w, len(boxes)], dtype='uint8')
-			# create masks
-			class_ids = list()
-			for i in range(len(boxes)):
-				box = boxes[i]
-				row_s, row_e = box[1], box[3]
-				col_s, col_e = box[0], box[2]
-				masks[row_s:row_e, col_s:col_e, i] = 1
-				class_ids.append(self.class_names.index('kidney'))
-		else:
-			class_ids = list()
-			masks = zeros([h, w], dtype='uint8')
-		return masks, asarray(class_ids, dtype='int32')
+    # extract bounding boxes from an annotation file
+    def extract_boxes(self, filename):
+        # load and parse the file
+        tree = ElementTree.parse(filename)
+        # get the root of the document
+        root = tree.getroot()
+        # extract each bounding box
+        boxes = list()
+        for box in root.findall('.//bndbox'):
+            xmin = int(box.find('xmin').text)
+            ymin = int(box.find('ymin').text)
+            xmax = int(box.find('xmax').text)
+            ymax = int(box.find('ymax').text)
+            coors = [xmin, ymin, xmax, ymax]
+            boxes.append(coors)
+        # extract image dimensions
+        width = int(root.find('.//size/width').text)
+        height = int(root.find('.//size/height').text)
+        return boxes, width, height
 
-	# load an image reference
-	def image_reference(self, image_id):
-		info = self.image_info[image_id]
-		return info['path']
+    # load the masks for an image
+    def load_mask(self, image_id):
+        # get details of image
+        info = self.image_info[image_id]
+        # define box file location
+        path = info['annotation']
+        # load XML
+        boxes, w, h = self.extract_boxes(path)
+        # create one array for all masks, each on a different channel
+        if len(boxes):
+            masks = zeros([h, w, len(boxes)], dtype='uint8')
+            # create masks
+            class_ids = list()
+            for i in range(len(boxes)):
+                box = boxes[i]
+                row_s, row_e = box[1], box[3]
+                col_s, col_e = box[0], box[2]
+                masks[row_s:row_e, col_s:col_e, i] = 1
+                class_ids.append(self.class_names.index('kidney'))
+        else:
+            class_ids = list()
+            masks = zeros([h, w], dtype='uint8')
+        return masks, asarray(class_ids, dtype='int32')
+
+    # load an image reference
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path']
 
 # train set
 train_set = KangarooDataset()
@@ -142,7 +145,7 @@ print('Test: %d' % len(test_set.image_ids))
 # else:
 #     for j in range(mask.shape[2]):
 #         pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.2)
-	
+    
 # pyplot.savefig("./kidney.png")
 
 # plot first few images
